25ada4cc881cdf97a393bfe715d97c669684cffc
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-11-20 18:07:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function createHouseJSON() {
+  return ({
+    "floors": [
+      {
+        "name": "First floor",
+        "rooms": [
+          {
+            "name": "Room 1"
+            "size": {"x": 100, "y": 100}
+          }
+        ]
+      }
+    ]
+  });
+}
+function getFloors(house) {
+  return house.floors;
+}
+function getRooms(floorLevel, house) {
+  return getFloors(house)[floorLevel - 1].rooms
+}
+var house = createHouseJSON();
